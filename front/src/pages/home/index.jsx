9250e1ecaf28c845a0e5f8d0c0426cb005a74c64
Leonardo de Souza Ruiz Bicalho <43609065877@>
2025-04-08 11:28:23 -0300

    08/04/2025

--- front/src/pages/home/index.jsx
+++ front/src/pages/home/index.jsx
@@ -1,125 +1,28 @@
 import React, { useState, useEffect } from "react";
-import { FaEdit, FaTrash, FaPlus, FaSearch } from 'react-icons/fa'
+import {FaUserGraduate, FaBook} from 'react-icons/fa'
 import Head from "../../components/head";
 import Footer from "../../components/footer";
 import { useNavigate } from 'react-router-dom'
-import axios from "axios";
-import './styles.css'
+import './home.css'
 
-export default function Disciplina() {
-    const [dados, setDados] = useState([])
-    const token = localStorage.getItem('token')
-    const [modalOpen, setModalOpen] = useState(false)
-    const [disciplinaSelecionada, setDisciplinaSelecionada] = useState(null)
-    const [setar, setSetar] = useState(false)
-
-    // console.log("Token Home: ", token)
-
-    useEffect(() => {
-        if (!token) return;
-
-        const fetchData = async () => {
-            try {
-                const response = await axios.get('http://127.0.0.1:8000/api/disc',
-                    {
-                        headers: {
-                            Authorization: `Bearer ${token}`
-                        }
-                    }
-                )
-                console.log(response.data)
-                setDados(response.data)
-
-            } catch (error) {
-                console.error(error)
-            }
-        }
-        fetchData()
-    }, [setar])
-
-    const editar = (disciplina) => {
-        console.log(disciplina)
-        setDisciplinaSelecionada(disciplina)
-        setModalOpen(true)
-    }
-
-
-    const deleteDisciplina = async (disciplina) => {
-        if (window.confirm("Tem certeza?")) {
-            try {
-                await axios.delete(`http://127.0.0.1:8000/api/disciplina/${disciplina.id}`,
-                    {
-                        headers: {
-                            Authorization: `Bearer ${token}`
-                        }
-                    }
-                )
-                console.log("Dados apagados com sucesso...")
-                setSetar(!setar)
-            } catch (error) {
-                console.error(error)
-            }
-        }
-    }
+export default function Home() {
+    const navigate = useNavigate();
 
     return (
-        <div className="container_disciplina">
-            <Head />
-            <section className="section-disciplina">
-                <div className="table-disciplina">
-                    <h2>Lista de disciplinas</h2>
-                    {dados.map((disciplina) => (
-                        <div className="lista">
-                            <div className="col1">
-                                <FaEdit className="edit" onClick={() => editar(disciplina)} />
-                            </div>
-                            <div className="col2">
-                                <FaTrash className="delete" onClick={() => deleteDisciplina(disciplina)} />
-                            </div>
-                            <div className="col3">
-                                <span className="id">{disciplina.id}</span>
-                            </div>
-                            <div className="col4">
-                                <span className="cod">{disciplina.cod}</span>
-                            </div>
-                            <div className="col5">
-                                <span className="disc">{disciplina.disc}</span>
-                            </div>
-                            <div className="col6">
-                                <span className="aulas">{disciplina.aulas}</span>
-                            </div>
-                        </div>
-                    ))
-
-                    }
+        <div className="main">
+            <header>
+                <Head/>
+            </header>
+            <div className="menu_container">
+                <div className="menu">
+                    <button onClick={navigate('/teacher')}> <FaUserGraduate/> Professores </button>
+                    <button onClick={navigate('/Disciplina')}> <FaBook/> Disciplina </button>
                 </div>
-                <div className="footer_table">
-                    <div className="btn1">
-                        <FaPlus
-                            className="adicionar"
-                            onClick={() => {
-                                setDisciplinaSelecionada(null),
-                                    setModalOpen(true)
-                            }}
-                        />
-                    </div>
-                    <div className="pesquisar">
-                        <input placeholder="ID" />
-                    </div>
-                    <div className="btn2">
-                        <FaSearch className="procurar" />
-                    </div>
-                </div>
-
-                <ModalDisciplinas
-                    isOpen={modalOpen}
-                    onClose={() => setModalOpen(false)}
-                    disciplinaSelecionada={disciplinaSelecionada}
-                    setSetar={setSetar}
-                    setar={setar}
-                />
-            </section>
-            <Footer />
+            </div>
+            <footer>
+                <Footer/>
+            </footer>
         </div>
     )
+
 }
--- front/src/pages/login/index.jsx
+++ front/src/pages/login/index.jsx
@@ -22,7 +22,7 @@ export default function Login() {
             )
             console.log("Token Login", response.data.access)
             localStorage.setItem('token', response.data.access)
-            navigate('/teacher')
+            navigate('/home')
         } catch (error) {
             console.error(error)
         }
